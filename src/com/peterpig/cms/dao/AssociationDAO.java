d88a6e4ec998d924cd093a7e5bdef9559452d1d8
JackieLiang <1343729568@qq.com>
2018-08-01 10:34:15 +0800

    Upload AssociationDAO.java

---
+++ src/com/peterpig/cms/dao/AssociationDAO.java
@@ -0,0 +1,11 @@
+package com.peterpig.cms.dao;
+
+import com.peterpig.cms.bean.Association;
+/**
+ * 社团接口类
+ * @author JackieL
+ *
+ */
+public interface AssociationDAO extends baseDAO<Association, Integer> {
+
+}
