76589dfe844a9db27b331b0b4a2769e2cb9a80ab
JackieLiang <1343729568@qq.com>
2018-08-02 21:19:11 +0800

    Upload dropdown.js bootstrap.js jquery.nivo.slider.js main.js jquery-1.9.0.min.js jquery-3.2.1.min.js switchPage.js npm.js bootstrap.min.js

---
+++ WebRoot/js/main.js
@@ -0,0 +1,13 @@
+{	
+	
+	$(document).ready(function(){//页面加载完之后，自动执行该方法
+		setTimeout(function(){$("#pre_logo").hide();},4000);//4秒后执行该方法
+});
+}
+{
+	$(document).ready(function () {
+    setTimeout(function () {
+        $("#body_contain").css("display","block");
+    }, 4500);
+});
+}
