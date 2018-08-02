ef92283cf4abf3fb33d2e61f67a9301f036e5fca
JackieLiang <1343729568@qq.com>
2018-08-02 23:16:20 +0800

    Upload AssociationDAOImpl.java

--- src/com/peterpig/cms/dao/impl/AssociationDAOImpl.java
+++ src/com/peterpig/cms/dao/impl/AssociationDAOImpl.java
@@ -1,10 +1,12 @@
 package com.peterpig.cms.dao.impl;
 
+import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
+import java.util.Set;
 
 import com.peterpig.cms.bean.Association;
 import com.peterpig.cms.bean.AuditStatus;
+import com.peterpig.cms.bean.Student;
 import com.peterpig.cms.dao.AssociationDAO;
 import com.peterpig.cms.util.OpenTransactionUtils;
 /**
@@ -16,18 +18,33 @@ public class AssociationDAOImpl extends OpenTransactionUtils implements
 		AssociationDAO {
 	
 	@Override
-	public boolean doCreat(Association bean) {
+	public boolean creatAssociation(Association bean,Integer studentId) {
 		boolean flag=false;  //默认返回值为false
 		super.openTransaction();   //开启事务，进行数据操作
 		try{
-			/**/
+			/* 如果要添加社团则需要先通过审核 */
 			Association association=new Association();
-			AuditStatus audiStatus=new AuditStatus();
+			AuditStatus status= (AuditStatus)session.get(AuditStatus.class, 1);//查询出未通过审核的数据
+			Student student=(Student)session.get(Student.class, studentId);
 			//将要添加的信息set进对象属性值中进行保存
 			association.setName(bean.getName());
-			
-			
+			association.setExplains(bean.getExplains());
+			association.setStatus(status);
+			student.setAssociation(association);
+			session.save(association);
+			session.save(student);
+			transaction.commit();
+		}catch(Exception e){
+			System.out.println("---------------daoImpl出现问题！--------------");
+			transaction.rollback();
+			e.printStackTrace();
 		}
+		return flag;
+	}
+	
+	@Override
+	public boolean doCreat(Association bean) {
+		// TODO Auto-generated method stub
 		return false;
 	}
 
