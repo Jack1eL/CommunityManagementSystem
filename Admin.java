bee7fc458f08a9a1ac5f5d700d13777a228d14b8
朱子千 <448663912@qq.com>
2018-08-02 22:10:16 +0800

    测试

---
+++ Admin.java
@@ -0,0 +1,38 @@
+package com.peterpig.cms.bean;
+/**
+ * 管理员
+ * @author Evan
+ *
+ */
+public class Admin {
+	/** 管理员编号 */
+	private Integer adminId;
+	/** 管理员用户名 */
+	private String username;
+	/** 管理员密码 */
+	private String password;
+	
+	private String test;
+	
+	/*----------------------------------GETTER AND SETTER------------------------------------------*/
+	
+	public Integer getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(Integer adminId) {
+		this.adminId = adminId;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+}
