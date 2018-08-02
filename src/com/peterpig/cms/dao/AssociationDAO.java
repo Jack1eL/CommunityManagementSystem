a8e2448dad57f1ebf701741ae3a1a3b46baad96f
JackieLiang <1343729568@qq.com>
2018-08-02 23:16:37 +0800

    Upload AssociationDAO.java

--- src/com/peterpig/cms/dao/AssociationDAO.java
+++ src/com/peterpig/cms/dao/AssociationDAO.java
@@ -1,11 +1,13 @@
 package com.peterpig.cms.dao;
 
 import com.peterpig.cms.bean.Association;
+import com.peterpig.cms.bean.Student;
 /**
  * 社团接口类
  * @author JackieL
  *
  */
 public interface AssociationDAO extends baseDAO<Association, Integer> {
-
+	
+	public boolean creatAssociation(Association bean,Integer studentId);
 }
