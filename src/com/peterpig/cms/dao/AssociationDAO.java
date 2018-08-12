0c382b728dfddcc74ed74da499b2097860170b22
JackieLiang <1343729568@qq.com>
2018-08-12 12:13:32 +0800

    新增社团接口类方法，申请社团的接口方法

--- src/com/peterpig/cms/dao/AssociationDAO.java
+++ src/com/peterpig/cms/dao/AssociationDAO.java
@@ -1,7 +1,6 @@
 package com.peterpig.cms.dao;
 
 import com.peterpig.cms.bean.Association;
-import com.peterpig.cms.bean.Student;
 /**
  * 社团接口类
  * @author JackieL
@@ -9,5 +8,11 @@ import com.peterpig.cms.bean.Student;
  */
 public interface AssociationDAO extends baseDAO<Association, Integer> {
 	
+	/**
+	 * 添加社团
+	 * @param bean 在社团表中添加纪录
+	 * @param studentId 关联某个学生所添加的社团
+	 * @return 添加成功返回true。添加失败返回false
+	 */
 	public boolean creatAssociation(Association bean,Integer studentId);
 }
