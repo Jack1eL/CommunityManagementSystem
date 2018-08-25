8cb96a8f56c60ddfe0d5926888e72cf87db184ef
陈一凡 <384493920@qq.com>
2018-08-25 12:33:48 +0800

    社团活动的业务逻辑接口

--- src/com/peterpig/cms/biz/AssociationActivityBiz.java
+++ src/com/peterpig/cms/biz/AssociationActivityBiz.java
@@ -24,9 +24,58 @@ public interface AssociationActivityBiz {
 	 * @param id 活动编号
 	 * @return 删除成功返回true，失败返回false
 	 */
-	public boolean updateActivity(Integer id);
+	public boolean deleteActivity(Integer id);
 	
-	public List<AssociationActivity> findAllActivity();
+	/**
+	 * 查询所有活动
+	 * 1、通过关键字模糊查询
+	 * 2、分页查询
+	 * 3、排序
+	 * @param keyWord 关键字
+	 * @param curPage 当前页
+	 * @param pageSize 页内记录数
+	 * @param orderType 排序类型
+	 * @param orderField 排序字段
+	 * @return 活动列表
+	 */
+	public List<AssociationActivity> findAllActivity(String keyWord,Integer curPage,Integer pageSize,String orderType,String orderField);
 	
+	/**
+	 * 查询某社团所有活动
+	 * 1、通过关键字模糊查询
+	 * 2、分页查询
+	 * 3、排序
+	 * @param keyWord 关键字
+	 * @param curPage 当前页
+	 * @param pageSize 页内记录数
+	 * @param orderType 排序类型
+	 * @param orderField 排序字段
+	 * @param associationId 所属社团编号
+	 * @return 活动列表
+	 */
+	public List<AssociationActivity> findAllActivityByAssociationId(String keyWord,Integer curPage,Integer pageSize,String orderType,String orderField,Integer associationId);
 	
+	/**
+	 * 通过活动编号查询活动详情
+	 * @param id 活动编号
+	 * @return 返回AssociationActivity对象，为null时未查询到
+	 */
+	public AssociationActivity findById(Integer activityId);
+	
+	/**
+	 * 查询所有的活动数
+	 * 1、关键字查询
+	 * @param keyWord 关键字
+	 * @return 活动的数目
+	 */
+	public Long getAllCount(String keyWord);
+	
+	/**
+	 * 查询某社团所有的活动数
+	 * 1、关键字查询
+	 * @param keyWord 关键字
+	 * @param associationId 社团编号
+	 * @return 活动的数目
+	 */
+	public Long getAllCount(String keyWord,Integer associationId);
 }
