d528ffbc2a77144d7b24c3e42035a2ac01440800
陈一凡 <384493920@qq.com>
2018-09-24 00:54:38 +0800

    Upload AssociationActivityDAOImpl.java

--- src/com/peterpig/cms/dao/impl/AssociationActivityDAOImpl.java
+++ src/com/peterpig/cms/dao/impl/AssociationActivityDAOImpl.java
@@ -134,7 +134,7 @@ public class AssociationActivityDAOImpl extends OpenTransactionUtils implements
 			Query query=session.createQuery(hql);
 			query.setString(0, "%"+keyWord+"%");
 			if(beanId==null){
-				query.setString(1, "");
+				query.setString(1, "%%");
 			}else{
 				query.setInteger(1, beanId);
 			}
