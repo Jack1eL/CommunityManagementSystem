fb0d6a445ef51a7a469cd21bcbe727a453586ff7
朱子千 <448663912@qq.com>
2018-09-25 18:51:56 +0800

    1

--- src/com/peterpig/cms/dao/StudentDAO.java
+++ src/com/peterpig/cms/dao/StudentDAO.java
@@ -3,5 +3,5 @@ package com.peterpig.cms.dao;
 import com.peterpig.cms.bean.Student;
 
 public interface StudentDAO extends baseDAO<Student, Integer> {
-
+	public Student findAllInfo(Student bean);
 }
