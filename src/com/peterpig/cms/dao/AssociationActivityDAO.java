d7ac6c93d86b9e3a89bc573c2c5edf86e255bb6e
陈一凡 <384493920@qq.com>
2018-09-30 22:17:46 +0800

    Upload AssociationActivityDAO.java

--- src/com/peterpig/cms/dao/AssociationActivityDAO.java
+++ src/com/peterpig/cms/dao/AssociationActivityDAO.java
@@ -1,7 +1,9 @@
 package com.peterpig.cms.dao;
 
+import java.util.List;
+
 import com.peterpig.cms.bean.AssociationActivity;
 
 public interface AssociationActivityDAO extends baseDAO<AssociationActivity, Integer> {
-	
+	public List<AssociationActivity> findAllByStatusId(String keyWord, Integer curPage, Integer pageSize, String orderType, String orderField, Integer beanId,Integer statusId);
 }
