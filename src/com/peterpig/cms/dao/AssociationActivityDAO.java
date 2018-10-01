21b7337d065ad8fe2360be0aa0dd17f6e2b7d774
陈一凡 <384493920@qq.com>
2018-10-01 17:29:14 +0800

    Upload AssociationActivityDAO.java

--- src/com/peterpig/cms/dao/AssociationActivityDAO.java
+++ src/com/peterpig/cms/dao/AssociationActivityDAO.java
@@ -6,4 +6,12 @@ import com.peterpig.cms.bean.AssociationActivity;
 
 public interface AssociationActivityDAO extends baseDAO<AssociationActivity, Integer> {
 	public List<AssociationActivity> findAllByStatusId(String keyWord, Integer curPage, Integer pageSize, String orderType, String orderField, Integer beanId,Integer statusId);
+	
+	/**
+	 * 修改状态
+	 * @param activityId 活动ID
+	 * @param statusId 状态
+	 * @return 修改成功true，失败false
+	 */
+	public boolean changeStatus(Integer activityId,Integer statusId);
 }
