69862fc3e5894d8c7963a4f49b6d37f3c8a124be
JackieLiang <1343729568@qq.com>
2018-10-01 23:32:14 +0800

    添加AssociationDAO接口里面的updateAssociaitonToPass()方法和updateAssociaitonToRefuse()方法

--- src/com/peterpig/cms/dao/AssociationDAO.java
+++ src/com/peterpig/cms/dao/AssociationDAO.java
@@ -15,4 +15,19 @@ public interface AssociationDAO extends baseDAO<Association, Integer> {
 	 * @return 添加成功返回true。添加失败返回false
 	 */
 	public boolean creatAssociation(Association bean,Integer studentId);
+	
+	/**
+	 * 通过某个社团
+	 * @param associationId 社团id
+	 * @return 通过成功返回true，通过失败返回false
+	 */
+	public boolean updateAssociationToPass(Integer associationId);
+	
+	/**
+	 * 拒绝某个社团的申请
+	 * @param associationId 社团id
+	 * @param studentId 申请的学生的id
+	 * @return 拒绝成功返回true，拒绝失败返回false
+	 */
+	public boolean updateAssociationToRefuse(Integer associationId,Integer studentId);
 }
